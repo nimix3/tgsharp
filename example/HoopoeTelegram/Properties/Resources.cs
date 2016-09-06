02c8d0e375913ea4aec1fef74fa96c68525f380d
NIMIX3 <nimix3@yahoo.com>
2016-09-07 02:25:15 +0430

    Add files via upload

---
+++ example/HoopoeTelegram/Properties/Resources.cs
@@ -0,0 +1,49 @@
+﻿namespace HoopoeTelegram.Properties
+{
+    using System;
+    using System.CodeDom.Compiler;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.Resources;
+    using System.Runtime.CompilerServices;
+
+    [DebuggerNonUserCode, GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0"), CompilerGenerated]
+    internal class Resources
+    {
+        private static CultureInfo resourceCulture;
+        private static System.Resources.ResourceManager resourceMan;
+
+        internal Resources()
+        {
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static CultureInfo Culture
+        {
+            get
+            {
+                return resourceCulture;
+            }
+            set
+            {
+                resourceCulture = value;
+            }
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static System.Resources.ResourceManager ResourceManager
+        {
+            get
+            {
+                if (resourceMan == null)
+                {
+                    System.Resources.ResourceManager manager = new System.Resources.ResourceManager("HoopoeTelegram.Properties.Resources", typeof(Resources).Assembly);
+                    resourceMan = manager;
+                }
+                return resourceMan;
+            }
+        }
+    }
+}
+
