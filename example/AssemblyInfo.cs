9b12b400baf601b26af9801c76938359abc8475c
NIMIX3 <nimix3@yahoo.com>
2016-09-06 22:05:18 +0430

    Add files via upload

---
+++ example/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+// Assembly TelegrampBulk, Version 1.0.0.0
+
+[assembly: AssemblyCopyright("Copyright \x00a9  2016 by Sheypoorak Co.")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Runtime.InteropServices.Guid("5f4f3d17-b841-4aae-a41e-62968e142609")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: AssemblyTrademark("Sheypoorak Co. (https://sheypoorak.com)")]
+//[assembly: System.Runtime.Versioning.TargetFramework(".NETFramework,Version=v4.5", FrameworkDisplayName=".NET Framework 4.5")]
+[assembly: AssemblyTitle("Hoopoe Telegram Account Manager (Free)")]
+[assembly: AssemblyDescription("Hoopoe Telegram Account Manager (Free)")]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sheypoorak Co. (https://sheypoorak.com)")]
+[assembly: AssemblyProduct("HTelegram Account Manager V.2")]
+[assembly: AssemblyVersion("2.0.0.0")]
+
